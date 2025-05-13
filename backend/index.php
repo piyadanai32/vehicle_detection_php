1e99fa3b652c587291dc77faa373339183765984
piyadanai32 <650112418059@bru.ac.th>
2025-05-13 13:41:37 +0700

    ++

--- backend/index.php
+++ backend/index.php
@@ -2,7 +2,7 @@
 
 $request_uri = $_SERVER['REQUEST_URI'];
 
-if (preg_match('#/vehicle_count/?$#', $request_uri)) {
+if (preg_match('#^/vehicle_count#', $request_uri)) {
     require __DIR__ . '/routes/vehicle.php';
     exit;
 }
--- backend/routes/vehicle.php
+++ backend/routes/vehicle.php
@@ -1,9 +1,36 @@
 <?php
 require_once __DIR__ . '/../controllers/VehicleController.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    VehicleController::store();
-} else {
-    http_response_code(405);
-    echo json_encode(['error' => 'Method not allowed']);
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$routes = [
+    'GET' => [
+
+    ],
+    'POST' => [
+        '#^/vehicle_count/?$#' => ['VehicleController', 'store'],
+    ],
+    'PUT' => [
+
+    ],
+    'DELETE' => [
+
+    ]
+];
+
+$matched = false;
+if (isset($routes[$method])) {
+    foreach ($routes[$method] as $pattern => $handler) {
+        if (preg_match($pattern, $uri)) {
+            call_user_func($handler);
+            $matched = true;
+            break;
+        }
+    }
+}
+
+if (!$matched) {
+    http_response_code(isset($routes[$method]) ? 404 : 405);
+    echo json_encode(['error' => isset($routes[$method]) ? 'Not found' : 'Method not allowed']);
 }
